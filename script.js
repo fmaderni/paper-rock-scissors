2246a3152a95bd95d128f746588b9d0b2524956e
tero27 <fmaderni@gmail.com>
2022-08-22 11:55:26 -0300

    added new functions and polished the previous ones. Game looking pretty good for being a console game

--- script.js
+++ script.js
@@ -16,21 +16,66 @@ function playerChoice(){
     return firstLet+rest
 }
 
-const compSelection = getComputerChoice()
-const playerSelection = 'Rock'
+const playerSelection = playerChoice()
 
 function playRound (playerSelection,compSelection) {
     if (playerSelection=='Rock' && compSelection=='Rock') {
         return "It's a tie!"
     }    
     else if (playerSelection=='Rock' && compSelection=='Paper') {
+        computerScore++
         return "Computer wins! You suck..."
     }
     else if (playerSelection=='Rock' && compSelection=='Scissors') {
+        playerScore++
         return "You win! The world is a step closer to being saved"
     }
-}    
-console.log(compSelection)
-console.log(playerSelection)
-console.log(playRound(playerSelection,compSelection))
+    if (playerSelection=='Paper' && compSelection=='Paper') {
+        return "It's a tie!"
+    }    
+    else if (playerSelection=='Paper' && compSelection=='Scissors') {
+        computerScore++
+        return "Computer wins! You suck..."
+    }
+    else if (playerSelection=='Paper' && compSelection=='Rock') {
+        playerScore++
+        return "You win! The world is a step closer to being saved"
+    }
+    if (playerSelection=='Scissors' && compSelection=='Scissors') {
+        return "It's a tie!"
+    }    
+    else if (playerSelection=='Scissors' && compSelection=='Rock') {
+        computerScore++
+        return "Computer wins! You suck..."
+    }
+    else if (playerSelection=='Scissors' && compSelection=='Paper') {
+        playerScore++
+        return "You win! The world is a step closer to being saved"
+    }
+
+} 
+
+function game() {
+    for (let i = 0; i < 5; i++){
+        
+        const compSelection = getComputerChoice()
+        console.log(playRound(playerSelection,compSelection))
+    
+    }
+}
+
+game()
+
+function score() {
+    if (playerScore < computerScore) {
+        return "Machines won.The world has been destroyed"
+    }
+    else if(playerScore > computerScore){
+        return "You saved the world! You're a hero!"
+    } 
+    else {
+        return "Play again you failure"
+    } 
+}
+console.log(score())
 
