0ad0875e16382e73a579af4077418a39681d2851
tero27 <fmaderni@gmail.com>
2022-08-22 21:45:53 -0300

    no changes really

--- script.js
+++ script.js
@@ -1,81 +1,80 @@
-let playerScore = 0
-let computerScore = 0
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
-    const arrOfOptions= ['Rock','Paper','Scissors']
-    const randomNumber = Math.floor(Math.random() * 3)
-    const compSelection = arrOfOptions[randomNumber]
-    return compSelection    
-}
+    const arrOfOptions = ['Rock', 'Paper', 'Scissors'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    const compSelection = arrOfOptions[randomNumber];
+    return compSelection;
+};
 
-function playerChoice(){
+function playerChoice() {
     var playerSelection = window.prompt("Enter your Choice: ");
-    const firstLet = playerSelection.charAt(0).toUpperCase()
-    const rest = playerSelection.slice(1).toLowerCase()
-    
-    return firstLet+rest
+    const firstLet = playerSelection.charAt(0).toUpperCase();
+    const rest = playerSelection.slice(1).toLowerCase();
+
+    return firstLet + rest;
+};
+
+const playerSelection = playerChoice();
+
+function playRound(playerSelection, compSelection) {
+    if (playerSelection == 'Rock' && compSelection == 'Rock') {
+        return "It's a tie!";
+    }
+    else if (playerSelection == 'Rock' && compSelection == 'Paper') {
+        computerScore++;
+        return "Computer wins! You suck...";
+    }
+    else if (playerSelection == 'Rock' && compSelection == 'Scissors') {
+        playerScore++;
+        return "You win! The world is a step closer to being saved";
+    }
+    if (playerSelection == 'Paper' && compSelection == 'Paper') {
+        return "It's a tie!";
+    }
+    else if (playerSelection == 'Paper' && compSelection == 'Scissors') {
+        computerScore++;
+        return "Computer wins! You suck...";
+    }
+    else if (playerSelection == 'Paper' && compSelection == 'Rock') {
+        playerScore++;
+        return "You win! The world is a step closer to being saved";
+    }
+    if (playerSelection == 'Scissors' && compSelection == 'Scissors') {
+        return "It's a tie!";
+    }
+    else if (playerSelection == 'Scissors' && compSelection == 'Rock') {
+        computerScore++;
+        return "Computer wins! You suck...";
+    }
+    else if (playerSelection == 'Scissors' && compSelection == 'Paper') {
+        playerScore++;
+        return "You win! The world is a step closer to being saved";
+    }
+
 }
-
-const playerSelection = playerChoice()
-
-function playRound (playerSelection,compSelection) {
-    if (playerSelection=='Rock' && compSelection=='Rock') {
-        return "It's a tie!"
-    }    
-    else if (playerSelection=='Rock' && compSelection=='Paper') {
-        computerScore++
-        return "Computer wins! You suck..."
-    }
-    else if (playerSelection=='Rock' && compSelection=='Scissors') {
-        playerScore++
-        return "You win! The world is a step closer to being saved"
-    }
-    if (playerSelection=='Paper' && compSelection=='Paper') {
-        return "It's a tie!"
-    }    
-    else if (playerSelection=='Paper' && compSelection=='Scissors') {
-        computerScore++
-        return "Computer wins! You suck..."
-    }
-    else if (playerSelection=='Paper' && compSelection=='Rock') {
-        playerScore++
-        return "You win! The world is a step closer to being saved"
-    }
-    if (playerSelection=='Scissors' && compSelection=='Scissors') {
-        return "It's a tie!"
-    }    
-    else if (playerSelection=='Scissors' && compSelection=='Rock') {
-        computerScore++
-        return "Computer wins! You suck..."
-    }
-    else if (playerSelection=='Scissors' && compSelection=='Paper') {
-        playerScore++
-        return "You win! The world is a step closer to being saved"
-    }
-
-} 
 
 function game() {
-    for (let i = 0; i < 5; i++){
-        
-        const compSelection = getComputerChoice()
-        console.log(playRound(playerSelection,compSelection))
-    
+    for (let i = 0; i < 5; i++) {
+
+        const compSelection = getComputerChoice();
+        console.log(playRound(playerSelection, compSelection));
+
     }
 }
 
-game()
+game();
 
 function score() {
     if (playerScore < computerScore) {
-        return "Machines won.The world has been destroyed"
+        return "Machines won.The world has been destroyed";
     }
-    else if(playerScore > computerScore){
-        return "You saved the world! You're a hero!"
-    } 
+    else if (playerScore > computerScore) {
+        return "You saved the world! You're a hero!";
+    }
     else {
-        return "Play again you failure"
-    } 
+        return "Play again you failure";
+    }
 }
-console.log(score())
-
+console.log(score());
