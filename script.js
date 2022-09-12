da46bcae2e45a784c4d54950797b2620bd3172b7
tero27 <fmaderni@gmail.com>
2022-09-12 16:31:54 -0300

    added some changes to html, some to js that I deleted. Gonna keep working on this script

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 
 let playerSelection;
 let computerSelection;
-let val;
 let btn = document.querySelectorAll(".button");
 let resultDiv = document.querySelector(".result");
 
+function playerChoice(button) {
+    playerSelection = button.value;
+    
+    const p = document.createElement('p');
+    p.innerText = playerSelection;
+    resultDiv.appendChild(p);
+};
 
-function getComputerSelection(){
 
-}
-
-
-
-
+function computerChoice() {
+    const arrOfChoices = ['Rock', 'Paper', 'Scissors'];
+    const random = Math.floor(Math.random() * 3);
+    computerSelection = arrOfChoices[random];
+    
+    const r = document.createElement('p');
+    r.innerText = computerSelection;
+    resultDiv.appendChild(r);
+};
 
 
 btn.forEach((button) => {
-    button.addEventListener('click',() => {
-        playerSelection = button.value;
-        const p = document.createElement('span');
-        p.innerText = playerSelection
-        resultDiv.appendChild(p);
+    button.addEventListener('click', () => {
+
+        playerChoice(button);
+        computerChoice();
+
     });
 });
-
 
